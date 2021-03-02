353f2d0d662e5ae5e59282ba00a6334aaf8542d5
SlothPete <Veerakit413@gmail.com>
2021-03-03 02:48:34 +0700

    update navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,49 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      'sm': '675px',
+    },
+    linearBorderGradients: {
+      directions: { // defaults to these values
+        't': 'to top',
+        'tr': 'to top right',
+        'r': 'to right',
+        'br': 'to bottom right',
+        'b': 'to bottom',
+        'bl': 'to bottom left',
+        'l': 'to left',
+        'tl': 'to top left',
+      },
+      colors: { // defaults to {}
+        'red': '#f00',
+        'red-blue': ['#f00', '#00f'],
+        'red-green-blue': ['#f00', '#0f0', '#00f'],
+        'black-white-with-stops': ['#000', '#000 45%', '#fff 55%', '#fff'],
+      },
+    },
+    repeatingLinearBorderGradients: theme => ({
+      directions: theme('linearBorderGradients.directions'), // defaults to the same values as linearBorderGradients’ directions
+      colors: theme('linearBorderGradients.colors'), // defaults to {}
+      lengths: { // defaults to {}
+        'sm': '25px',
+        'md': '50px',
+        'lg': '100px',
+      },
+    }),
     extend: {},
+    
   },
   variants: {
-    extend: {},
+    linearBorderGradients: ['responsive'], // defaults to ['responsive']
+    repeatingLinearBorderGradients: ['responsive'], // defaults to ['responsive']
+    extend: {
+      transitionDuration: ['hover', 'focus'],
+    },
   },
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
+    require('tailwindcss-border-gradients')(),
   ],
 }
