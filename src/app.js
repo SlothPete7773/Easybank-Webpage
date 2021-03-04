31cb924502f3548f48fb3e032dfe2fa163d37a0e
SlothPete <Veerakit413@gmail.com>
2021-03-05 00:10:01 +0700

    Update navbar animation

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
-console.log("Hello World");
+
+const hamburgerMenu = () => {
+  let overlay = document.getElementById('overlay');
+  let content = document.getElementById('hamburger-content');
+  let menu = document.getElementById('hamburger-menu')
+  let status = menu.classList.contains('isOpen')
+  
+  if (status) {
+    menu.classList.remove('isOpen');
+    overlay.classList.add('hidden');
+    content.classList.add('hidden');
+  } else {
+    menu.classList.add('isOpen');
+    overlay.classList.remove('hidden');
+    content.classList.remove('hidden');
+  }
+}
+
+// function hamburgerMenu() {
+
+// }
