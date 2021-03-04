ed6cd4de77167ece8a9e417ba6df0576a286f8f6
SlothPete <Veerakit413@gmail.com>
2021-03-05 01:41:16 +0700

    Update no-scroll when open navbar

--- src/app.js
+++ src/app.js
@@ -3,16 +3,19 @@ const hamburgerMenu = () => {
   let overlay = document.getElementById('overlay');
   let content = document.getElementById('hamburger-content');
   let menu = document.getElementById('hamburger-menu')
+  let body = document.querySelector('body');
   let status = menu.classList.contains('isOpen')
   
   if (status) {
     menu.classList.remove('isOpen');
     overlay.classList.add('hidden');
     content.classList.add('hidden');
+    body.classList.remove('overflow-hidden');
   } else {
     menu.classList.add('isOpen');
     overlay.classList.remove('hidden');
     content.classList.remove('hidden');
+    body.classList.add('overflow-hidden');
   }
 }
 
